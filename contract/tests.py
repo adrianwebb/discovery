4f1d5e32e67a7ff1cf710bad004920944f09e253
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-18 21:20:01 -0500

    Updates to the tests.

--- contract/tests.py
+++ contract/tests.py
@@ -4,7 +4,7 @@ from django.core.management import call_command
 
 class FPDSLoaderTest(TestCase):
     """Tests the load_fpds management command"""
-    fixtures = ['naics.json', 'setasides.json', 'vendors.json']
+    fixtures = ['naics.json', 'setasides.json', 'fpds_vendor.json']
 
     def test_load(self):
         call_command('load_fpds', years=1, weeks=52, pause=0)        
